281574457aec410f703236dd32379aee393c83b3
BastienD <biloute.bd@gmail.com>
2024-03-24 14:31:26 +0100

    error

--- src/pages/error/errorPage.jsx
+++ src/pages/error/errorPage.jsx
@@ -1,6 +1,16 @@
 import './error.scss'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 function ErrorPage() {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      navigate("/");
+    }, [navigate]);
+
     return(
         <div className='404Container'>
             <span>404</span>
