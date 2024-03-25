dd6af7b94c36b948183ea540184b108bdc5a8bc0
BastienD <biloute.bd@gmail.com>
2024-03-25 15:49:40 +0100

    Adjustments

--- src/assets/data/projectList.js
+++ src/assets/data/projectList.js
@@ -1,4 +1,27 @@
-import { techno } from "./technos"
+import { techno } from "./technos";
+import argentbankCover from '../data/images/argentbank/cover.png'
+import argentbankimg1 from '../data/images/argentbank/img1.png'
+import argentbankimg2 from '../data/images/argentbank/img2.png'
+
+import bookiCover from '../data/images/booki/cover.png'
+import bookiimg1 from '../data/images/booki/img1.png'
+import bookiimg2 from '../data/images/booki/img2.png'
+import bookiimg3 from '../data/images/booki/img3.png'
+
+import kasaCover from '../data/images/kasa/cover.png'
+import kasaImg1 from '../data/images/kasa/img1.png'
+import kasaImg2 from '../data/images/kasa/img2.png'
+
+import ninaCover from '../data/images/ninacarducci/cover.png'
+import ninaImg1 from '../data/images/ninacarducci/img1.png'
+import ninaImg2 from '../data/images/ninacarducci/img2.png'
+
+import sophieCover from '../data/images/sophiebluel/cover.png'
+import sophieImg1 from '../data/images/sophiebluel/img1.png'
+import sophieImg2 from '../data/images/sophiebluel/img2.png'
+import sophieImg3 from '../data/images/sophiebluel/img3.png'
+
+
 
 export const projectsList =  [
       {
@@ -7,8 +30,8 @@ export const projectsList =  [
         "summary": "Bank account interface",
         "description": "ArgentBank, a comprehensive banking interface, consolidates user accounts. My task focused on managing user authentication with Redux while ensuring robust security measures for connections. Through meticulous implementation, the project fortified user authentication protocols, enhancing the platform's reliability and user trust.",
         "technologies": [techno.react, techno.js, techno.sass],
-        "coverImage": "/src/assets/data/images/cover.png",
-        "illustrationImages": ["/src/assets/data/images/argentbank/cover.png", "/src/assets/data/images/argentbank/img1.png", "/src/assets/data/images/argentbank/img2.png"],
+        "coverImage": argentbankCover,
+        "illustrationImages": [argentbankCover, argentbankimg1, argentbankimg2],
         "externalLinks": {
           "VIEW CODE": "https://github.com/JksonKnew/P11",
         }
@@ -19,8 +42,8 @@ export const projectsList =  [
         "summary": "Logement search start-up",
         "description": "For Booki, a startup aimed at creating an interface for users to search for accommodations in specific locations, I undertook the task of developing this dynamic interface. My mission involved crafting an intuitive platform that enables users to seamlessly search for lodging in precise locations, enhancing their overall experience and facilitating efficient property exploration.",
         "technologies": [techno.html, techno.css, techno.figma],
-        "coverImage": "/src/assets/data/images/booki/cover.png",
-        "illustrationImages": ["/src/assets/data/images/booki/cover.png","/src/assets/data/images/booki/img1.png", "/src/assets/data/images/booki/img2.png", "/src/assets/data/images/booki/img3.png"],
+        "coverImage": bookiCover,
+        "illustrationImages": [bookiCover, bookiimg1, bookiimg2, bookiimg3],
         "externalLinks": {
           "VIEW CODE": "https://github.com/JksonKnew/P3",
           "LIVE VIEW": "https://jksonknew.github.io/P3/",
@@ -32,8 +55,8 @@ export const projectsList =  [
         "summary": "Apartment rental between individuals",
         "description": "Kasa is a web application for peer-to-peer real estate rental.The aim of my mission was to dynamically integrate the website based on a Figma mockup. Leveraging a database, the site automatically manages the pages.",
         "technologies": [techno.react, techno.sass, techno.js, techno.figma],
-        "coverImage": "/src/assets/data/images/kasa/cover.png",
-        "illustrationImages": ["/src/assets/data/images/kasa/cover.png", "/src/assets/data/images/kasa/img1.png", "/src/assets/data/images/kasa/img2.png"],
+        "coverImage": kasaCover,
+        "illustrationImages": [kasaCover, kasaImg1, kasaImg2],
         "externalLinks": {
           "VIEW CODE": "https://github.com/JksonKnew/P8",
         }
@@ -44,8 +67,8 @@ export const projectsList =  [
         "summary": "Administrator interface",
         "description": "For Sophie Bluel's portfolio, a architect's showcase, I developed an administration interface enabling dynamic management of the photos displayed in her portfolio. I integrated JavaScript for authentication, ensuring secure access to the platform's backend. This allowed Sophie to effortlessly update and organize her photography collection, enhancing the overall user experience for visitors to her portfolio.",
         "technologies": [techno.html, techno.sass, techno.js, techno.figma],
-        "coverImage": "/src/assets/data/images/sophiebluel/cover.png",
-        "illustrationImages": ["/src/assets/data/images/sophiebluel/cover.png", "/src/assets/data/images/sophiebluel/img1.png", "/src/assets/data/images/sophiebluel/img2.png","/src/assets/data/images/sophiebluel/img3.png"],
+        "coverImage": sophieCover,
+        "illustrationImages": [sophieImg1, sophieImg2, sophieImg3],
         "externalLinks": {
           "VIEW CODE": "https://github.com/JksonKnew/P6",
         }
@@ -56,8 +79,8 @@ export const projectsList =  [
         "summary": "SEO optimization",
         "description": "For Nina Carducci's photography portfolio, I enhanced SEO optimization and crafted an optimization report. My role involved implementing strategies to boost visibility and reach on search engines, ensuring her work receives the attention it deserves.",
         "technologies": [techno.html, techno.sass, techno.js],
-        "coverImage": "/src/assets/data/images/ninacarducci/cover.png",
-        "illustrationImages": ["/src/assets/data/images/ninacarducci/cover.png", "/src/assets/data/images/ninacarducci/img1.png", "/src/assets/data/images/ninacarducci/img2.png"],
+        "coverImage": ninaCover,
+        "illustrationImages": [ninaCover, ninaImg1, ninaImg2],
         "externalLinks": {
           "VIEW CODE": "https://github.com/JksonKnew/P9",
           "LIVE VIEW": "https://jksonknew.github.io/P9/",
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  base:"/PF/",
+  base:"/",
   plugins: [react()]
 })
